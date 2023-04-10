9fb129547c17e7180fd285cd89f09be8b2d9a863
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-04-10 22:24:28 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    // Write your algorithm here
+  let palindrome=word.split('').reverse().join('')
+  if(palindrome===word){
+    return true
+  }
+  else{
+      return false
+  }
 }
 
-/* 
+/* Create a variable palindrome 
+ Create if statement 
+ return true 
   Add your pseudocode here
 */
 
 /*
+Create a varible palindrome to split the word then reverse it then join the word 
+Add an if statement and pass an argument condition  and return true if the condition is met
+else return false 
   Add written explanation of your solution here
 */
 
